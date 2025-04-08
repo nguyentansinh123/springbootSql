99c825d43c27fa7cf3a311e7dcf26aaaafa29070
sinh <nsinh2281@gmail.com>
2025-04-08 15:43:05 +1000

    commit

--- src/main/java/com/example/dreamshop/service/cart/CartService.java
+++ src/main/java/com/example/dreamshop/service/cart/CartService.java
@@ -2,6 +2,7 @@ package com.example.dreamshop.service.cart;
 
 import com.example.dreamshop.exceptions.ResourceNotFoundException;
 import com.example.dreamshop.model.Cart;
+import com.example.dreamshop.repository.CartItemRepository;
 import com.example.dreamshop.repository.CartRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,17 +13,21 @@ import java.math.BigDecimal;
 @RequiredArgsConstructor
 public class CartService implements ICartService {
     private final CartRepository cartRepository;
+    private final CartItemRepository cartItemRepository;
 
     @Override
     public Cart getCart(Long id) {
         Cart cart = cartRepository.findById(id)
                 .orElseThrow(()-> new ResourceNotFoundException("Cart not found"));
         BigDecimal totalAmount = cart.getTotalAmount();
-        return null;
+        cart.setTotalAmount(totalAmount);
+        return cartRepository.save(cart);
     }
 
     @Override
     public void clearCart(Long id) {
+
+        Cart cart = getCart(id);
 
     }
 
